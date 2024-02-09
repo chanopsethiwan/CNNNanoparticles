f64d1147d7955a0f82e692f762ca75864b8efcce
chanopsethiwan <chanopsethiwan233@gmail.com>
2024-02-09 01:37:41 +0000

    update file manager

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-import torch
-a = torch.tensor([1,2,3])
-b = torch.tensor([4,5,6])
-print(torch.stack((a,b)).shape)
+import os
+input_folder = r"C:\Users\Public\PartIIB project 2023_2024\Image collection without reaction\00MultiConc\full_trimmed_folder2"
+# input_folder = r"C:\Users\Public\PartIIB project 2023_2024\PastData\AgNO3Helical_size"
+# input_folder = r"C:\Users\Public\PartIIB project 2023_2024\PastData\MultiConcHelical_size"
+# output_folder = r"C:\Users\Public\PartIIB project 2023_2024\Image collection without reaction\00MultiConc\full_trimmed_folder3"
+# output_folder = r"C:\Users\Public\PartIIB project 2023_2024\PastData\AgNO3Helical_size\third_quarter"
+# output_folder = r"C:\Users\Public\PartIIB project 2023_2024\PastData\MultiConcHelical_size\first_quarter"
+print(os.listdir(input_folder))
